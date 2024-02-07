56df0392fcff6f9e0228004225a25eaf7a74877e
M-Ridha <82166718+M-Ridha@users.noreply.github.com>
2024-02-07 11:27:40 +0100

    create fakeData of users

---
+++ FakeData.js
@@ -0,0 +1,37 @@
+export const UserList = [
+  {
+    id: 1,
+    name: "John Doe",
+    userName: "johndoe87",
+    age: 35,
+    nationality: "American",
+  },
+  {
+    id: 2,
+    name: "Jane Smith",
+    userName: "janesmith23",
+    age: 28,
+    nationality: "British",
+  },
+  {
+    id: 3,
+    name: "Ahmed Hassan",
+    userName: "ahmedhassan",
+    age: 42,
+    nationality: "Egyptian",
+  },
+  {
+    id: 4,
+    name: "Maria Garcia",
+    userName: "mariag",
+    age: 30,
+    nationality: "Spanish",
+  },
+  {
+    id: 5,
+    name: "Liu Wei",
+    userName: "liuwei88",
+    age: 25,
+    nationality: "Chinese",
+  },
+];
