bfcfcba801870f77b7f3e1442fa1ce046ef6ff94
M-Ridha <82166718+M-Ridha@users.noreply.github.com>
2024-02-07 11:24:09 +0100

    create user query

---
+++ schemas/type-defs.js
@@ -0,0 +1,18 @@
+const {gql} = require ('apollo-server')
+
+
+//1 - create query (users) this query we gonna retrieve the list of users instance from type User
+
+const typeDefs = gql`
+    
+    type User {
+        name: String!
+        userName: String!
+        age: Int!
+        nationality: String!
+    }
+    
+    type Query {
+        users: [User!]!
+    }
+`
