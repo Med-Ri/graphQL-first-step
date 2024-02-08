e70a9b86c1195da7cf3be0ba753e6a2203064bc9
M-Ridha <82166718+M-Ridha@users.noreply.github.com>
2024-02-08 22:43:29 +0100

    settings client side

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+const URI = process.env.URI;
 
 function App() {
+  const client = new ApolloClient({
+    cache: new InMemoryCache(),
+    uri: URI,
+  });
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <div className="App">
+        <h1> List of users </h1>
+      </div>
+    </ApolloProvider>
   );
 }
 
