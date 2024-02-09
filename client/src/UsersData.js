6647eeb2602879e15b77297f45bf33f65010a6ad
M-Ridha <82166718+M-Ridha@users.noreply.github.com>
2024-02-09 13:35:04 +0100

    add new user [ client side ]

--- client/src/UsersData.js
+++ client/src/UsersData.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { useQuery, gql } from "@apollo/client";
+import React, { useState } from "react";
+import { useQuery, gql, useMutation } from "@apollo/client";
 
 const QUERY_ALL_USERS = gql`
   query GetAllUsers {
@@ -12,8 +12,34 @@ const QUERY_ALL_USERS = gql`
   }
 `;
 
+const CREATE_USER_MUTATION = gql`
+  mutation CreateUser ($input: CreateUserInput! ) {
+    createUser(input: $input){
+      name
+      id
+    }
+  
+  
+  }
+
+
+
+
+`
+
 const UsersData = () => {
   const { data, loading , error } = useQuery(QUERY_ALL_USERS);
+
+  const [name , setName] = useState('');
+  const [userName, setUserName] = useState('');
+  const [age , setAge] = useState(0)
+  const [nationality, setNationality] = useState('')
+
+
+  const [createNewUser] = useMutation(CREATE_USER_MUTATION)
+
+
+
 
   if (loading) {
     return <h1> LOADING...  </h1>
@@ -26,6 +52,58 @@ const UsersData = () => {
 
   return( 
     <div>
+        <div className="bg-warning p-2">
+          <h1> Add New User </h1>
+
+          <div className="d-flex flex-column container-sm w-25">
+            <input 
+              className="my-2" 
+              type="text" 
+              placeHolder = "Name..." 
+              onChange={(e) => setName(e.target.value)}  
+            />
+            <input 
+              className="my-2" 
+              type="text" 
+              placeHolder = "User_Name..." 
+              onChange={(e) => setUserName(e.target.value)}    
+            />
+            <input 
+              className="my-2" 
+              type="number" 
+              min={0} 
+              placeHolder = "Age..." 
+              onChange={(e) => setAge(parseInt(e.target.value))}   
+            />
+            
+            <input 
+              className="my-2" 
+              type="text" 
+              placeHolder = "Nationality..." 
+              onChange={(e) => setNationality(e.target.value.toUpperCase())}   
+            />
+            
+            
+            <button 
+              onClick={()=>{
+                createNewUser({variables: {input : {
+                  name,
+                  userName,
+                  age,
+                  nationality
+                }}})
+              }}
+            > 
+              Add New User 
+            </button>
+          </div>
+
+
+        </div>
+
+       
+
+
         <h1> List of users </h1>
         {
             data && data?.users?.map ((user , i) => {
